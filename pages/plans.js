c728554be6ce406b2299277042f94963fea4e0cc
IbrahimSam96 <Ibrahimsam96@gmail.com>
2023-05-07 18:43:00 +0300

    ASa

--- pages/plans.js
+++ pages/plans.js
@@ -425,7 +425,7 @@ const Plans = ({ email, uid }) => {
                   <span
                     onClick={() => { }}
                     className={`transition-[margin] py-2 px-[25.5px] sm:px-[36px] 
-                    ${numberOfRecipes == 3 ? `mr-[116.5px] sm:mr-[158px]` : numberOfRecipes == 4 ? `mr-[58.3px] sm:mr-[79px] ` : `mr-0`} 
+                    ${numberOfRecipes == 3 ? `mr-[116.5px] sm:mr-[158px]` : numberOfRecipes == 4 ? `mr-[58.4px] sm:mr-[79px] ` : `mr-0`} 
                     border-[1px] border-[green]  bg-[#056835] `}>
                     <p className={`text-[white] text-[0.7em] font-bold inline`}>
                       {numberOfRecipes}
