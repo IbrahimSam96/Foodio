86c70256e8a8b209e275f64c79b44ebaac297086
IbrahimSam96 <Ibrahimsam96@gmail.com>
2023-05-07 18:00:14 +0300

    Responsive

--- pages/login.js
+++ pages/login.js
@@ -126,7 +126,7 @@ const Login = () => {
 
     return (
 
-        <div className={`h-full min-h-screen w-full grid grid-cols-[repeat(7,1fr)] grid-rows-[60px,750px,350px] bg-[#F8F8F8]`}>
+        <div className={`h-full min-h-screen w-full grid grid-cols-[repeat(7,1fr)] grid-rows-[60px,550px,350px] bg-[#F8F8F8]`}>
 
             <div className={`bg-[#FFFFFF] mx-2 fixed w-full z-30 col-start-1 col-end-8 grid grid-cols-[60px,100px,auto,auto,auto,150px] sm:grid-cols-[60px,100px,160px,160px,auto,150px] shadow shadow-slate-300 max-h-[64px]`}>
 
@@ -414,6 +414,7 @@ const Login = () => {
                 </div>
             }
             <div className={`col-start-1 col-end-8 row-start-3 row-end-4 grid bg-[#FFFFFF] grid-cols-[repeat(7,1fr)]`}>
+
                 <div className="justify-self-center sm:justify-self-auto col-start-1 sm:col-start-2 col-end-7 row-start-3 row-end-4 grid grid-cols-1 bg-[#FFFFFF] ">
 
                     <span className={`flex self-center`}>
@@ -489,21 +490,21 @@ const Login = () => {
 
                             <Link href={'/conditions'}>
 
-                                <p className={`text-[0.8em] font-medium font-serif text-[green] p-2 underline whitespace-nowrap`}>
+                                <p className={`hidden sm:block text-[0.8em] font-medium font-serif text-[green] p-2 underline whitespace-nowrap`}>
                                     Terms & Conditions
                                 </p>
                             </Link>
 
                             <Link href={'/privacy'}>
 
-                                <p className={`ml-4 text-[0.8em] font-medium font-serif text-[green] p-2 underline`}>
+                                <p className={`hidden sm:block ml-4 text-[0.8em] font-medium font-serif text-[green] p-2 underline`}>
                                     Privacy
                                 </p>
                             </Link>
 
                         </span>
 
-                        <span className={`flex my-auto ml-auto`}>
+                        <span className={`flex my-auto ml-auto `}>
 
                             <svg className={`mx-2`} width="25" height="25" viewBox="0 0 48 48" fill="#1877F2" xmlns="http://www.w3.org/2000/svg" aria-labelledby="facebook"><title id="facebook">facebook</title><path d="M48 24C48 10.745 37.255 0 24 0S0 10.745 0 24c0 11.98 8.776 21.908 20.25 23.708v-16.77h-6.094V24h6.094v-5.288c0-6.014 3.583-9.337 9.065-9.337 2.626 0 5.372.469 5.372.469v5.906h-3.026c-2.981 0-3.911 1.85-3.911 3.748V24h6.656l-1.064 6.938H27.75v16.77C39.224 45.908 48 35.98 48 24"></path><path fill="#fff" d="M33.342 30.938L34.406 24H27.75v-4.502c0-1.898.93-3.748 3.911-3.748h3.026V9.844s-2.746-.469-5.372-.469c-5.482 0-9.065 3.322-9.065 9.337V24h-6.094v6.938h6.094v16.77a24.174 24.174 0 007.5 0v-16.77h5.592z" class="facebook"></path></svg>
 
@@ -511,8 +512,6 @@ const Login = () => {
                         </span>
 
                     </span>
-
-
 
                 </div>
             </div>
